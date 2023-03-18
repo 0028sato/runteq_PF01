8d0d53b08452928a0f6c1b2a528d95ada836bc5a
yusuke sato <apple45stt@gmail.com>
2023-03-19 08:18:37 +0900

    バリデーションの設定の変更

--- app/models/snow_board.rb
+++ app/models/snow_board.rb
@@ -1,4 +1,6 @@
 class SnowBoard < ApplicationRecord
   has_many :post_board_relations, dependent: :destroy, foreign_key: 'snow_board_id'
   has_many :posts, through: :post_board_relations
+
+  validates :board_name, presence: true
 end
--- app/models/snow_shoe.rb
+++ app/models/snow_shoe.rb
@@ -1,4 +1,6 @@
 class SnowShoe < ApplicationRecord
     has_many :post_shoe_relations, dependent: :destroy, foreign_key: 'snow_shoe_id'
     has_many :posts, through: :post_shoe_relations
+
+    validates :shoe_name, presence: true
 end
